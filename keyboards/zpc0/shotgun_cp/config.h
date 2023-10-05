97d5cab461dbbaf81b473b41045544b8241615ba
Yanase Yuki <dev@zpc.st>
2023-10-05 18:27:21 +0900

    add ShotgunCherryPie zpc version

---
+++ keyboards/zpc0/shotgun_cp/config.h
@@ -0,0 +1,8 @@
+// Copyright 2021 Hayashi (@w_vwbw)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define RGB_MATRIX_LED_COUNT 48
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
